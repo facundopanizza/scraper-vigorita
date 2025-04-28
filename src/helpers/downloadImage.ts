4fb8a9a371ccad032d65794f8dbdb1576116672a
Facundo Panizza <facundo@panizza.dev>
2025-04-28 12:08:50 -0300

    thingy

--- src/helpers/downloadImage.ts
+++ src/helpers/downloadImage.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 import fs from 'fs/promises'
+import sharp from "sharp";
 
 export const downloadImage = async (src: string, filename: string) => {
   const response = await axios(src, { responseType: 'arraybuffer' });
 
-  await fs.writeFile(filename, response.data);
+  await sharp(response.data).jpeg({
+    quality: 80
+  }).toFile(filename);
+
+  // await fs.writeFile(filename, response.data);
 }
